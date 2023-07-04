f3707faec12c8e1c515f16af81a444daba982d3f
xjh2000 <simple.xjh@qq.com>
2023-07-04 10:03:02 +0800

    [Project] change structure

---
+++ src/led.v
@@ -0,0 +1,329 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/06/2023 04:01:17 PM
+// Design Name: 
+// Module Name: led
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module led (
+    input clk,
+    input en,
+    input [31:0] data,
+    output reg CA,
+    output reg CB,
+    output reg CC,
+    output reg CD,
+    output reg CE,
+    output reg CF,
+    output reg CG,
+    output DP,
+    output reg [7:0] AN
+);
+  parameter CLK_FREQUENCY = 100_000_000;
+  localparam MAX_COUNTER = CLK_FREQUENCY / 1000;  // 1ms needs number of counter  
+
+  reg [31:0] counter;  // counter for 1ms
+  reg        one_ms;  // 1ms one_ms
+  reg [ 7:0] counter_AN;  // counter for AN enable , counter 0-15 for 8 AN
+  reg [ 5:0] number_display;  // display number
+
+  assign DP = 1'b1;
+  
+  // use counter to creat 1ms delay signl
+  always @(posedge clk) begin
+    if (counter < MAX_COUNTER - 1) begin
+      counter <= counter + 1'b1;
+      one_ms  <= 1'b0;
+    end else begin
+      counter <= 32'b0;
+      one_ms  <= 1'b1;
+    end
+  end
+
+  // change counter_AN for one_ms
+  always @(posedge clk) begin
+    if (one_ms) begin
+      if (counter_AN < 8'd15) counter_AN <= counter_AN + 1'b1;
+      else counter_AN <= 8'b0;
+    end
+  end
+
+  // change AN and number_display driver by one_ms (1ms)
+  always @(posedge clk) begin
+    if (en) begin
+      case (counter_AN)
+        // 0 index
+        8'd0: begin
+          AN <= 8'b01111111;
+          number_display <= {1'b0, data[31:28]};
+        end
+        8'd1: begin
+          AN <= 8'b11111111;
+          number_display <= 5'd16;
+        end
+        // 1 index
+        8'd2: begin
+          AN <= 8'b10111111;
+          number_display <= {1'b0, data[27:24]};
+        end
+        8'd3: begin
+          AN <= 8'b11111111;
+          number_display <= 5'd16;
+        end
+        // 2 index
+        8'd4: begin
+          AN <= 8'b11011111;
+          number_display <= {1'b0, data[23:20]};
+        end
+        8'd5: begin
+          AN <= 8'b11111111;
+          number_display <= 5'd16;
+        end
+        // 3 index
+        8'd6: begin
+          AN <= 8'b11101111;
+          number_display <= {1'b0, data[19:16]};
+        end
+        8'd7: begin
+          AN <= 8'b11111111;
+          number_display <= 5'd16;
+        end
+        // 4 index
+        8'd8: begin
+          AN <= 8'b11110111;
+          number_display <= {1'b0, data[15:12]};
+        end
+        8'd9: begin
+          AN <= 8'b11111111;
+          number_display <= 5'd16;
+        end
+        // 5 index
+        8'd10: begin
+          AN <= 8'b11111011;
+          number_display <= {1'b0, data[11:8]};
+        end
+        8'd11: begin
+          AN <= 8'b11111111;
+          number_display <= 5'd16;
+        end
+        // 6 index
+        8'd12: begin
+          AN <= 8'b11111101;
+          number_display <= {1'b0, data[7:4]};
+        end
+        8'd13: begin
+          AN <= 8'b11111111;
+          number_display <= 5'd16;
+        end
+        // 7 index
+        8'd14: begin
+          AN <= 8'b11111110;
+          number_display <= {1'b0, data[3:0]};
+        end
+        8'd15: begin
+          AN <= 8'b11111111;
+          number_display <= 5'd16;
+        end
+      endcase
+
+    end else begin
+      AN <= 8'b11111111;
+      number_display <= 5'd16;
+    end
+  end
+
+  always @(posedge clk) begin
+    case (number_display)
+      5'd0: begin
+        // b1000000
+        CG <= 1'b1;
+        CF <= 1'b0;
+        CE <= 1'b0;
+        CD <= 1'b0;
+        CC <= 1'b0;
+        CB <= 1'b0;
+        CA <= 1'b0;
+
+      end
+      5'd1: begin
+        // b1111001
+        CG <= 1'b1;
+        CF <= 1'b1;
+        CE <= 1'b1;
+        CD <= 1'b1;
+        CC <= 1'b0;
+        CB <= 1'b0;
+        CA <= 1'b1;
+      end
+      5'd2: begin
+        // b0100100
+        CG <= 1'b0;
+        CF <= 1'b1;
+        CE <= 1'b0;
+        CD <= 1'b0;
+        CC <= 1'b1;
+        CB <= 1'b0;
+        CA <= 1'b0;
+      end
+      5'd3: begin
+        // b0110000
+        CG <= 1'b0;
+        CF <= 1'b1;
+        CE <= 1'b1;
+        CD <= 1'b0;
+        CC <= 1'b0;
+        CB <= 1'b0;
+        CA <= 1'b0;
+
+      end
+      5'd4: begin
+        // b0011001
+        CG <= 1'b0;
+        CF <= 1'b0;
+        CE <= 1'b1;
+        CD <= 1'b1;
+        CC <= 1'b0;
+        CB <= 1'b0;
+        CA <= 1'b1;
+      end
+      5'd5: begin
+        // b0010010
+        CG <= 1'b0;
+        CF <= 1'b0;
+        CE <= 1'b1;
+        CD <= 1'b0;
+        CC <= 1'b0;
+        CB <= 1'b1;
+        CA <= 1'b0;
+      end
+      5'd6: begin
+        // b0000010
+        CG <= 1'b0;
+        CF <= 1'b0;
+        CE <= 1'b0;
+        CD <= 1'b0;
+        CC <= 1'b0;
+        CB <= 1'b1;
+        CA <= 1'b0;
+
+      end
+      5'd7: begin
+        // b1111000
+        CG <= 1'b1;
+        CF <= 1'b1;
+        CE <= 1'b1;
+        CD <= 1'b1;
+        CC <= 1'b0;
+        CB <= 1'b0;
+        CA <= 1'b0;
+      end
+      5'd8: begin
+        // b0000000
+        CG <= 1'b0;
+        CF <= 1'b0;
+        CE <= 1'b0;
+        CD <= 1'b0;
+        CC <= 1'b0;
+        CB <= 1'b0;
+        CA <= 1'b0;
+      end
+      5'd9: begin
+        // b0010000
+        CG <= 1'b0;
+        CF <= 1'b0;
+        CE <= 1'b1;
+        CD <= 1'b0;
+        CC <= 1'b0;
+        CB <= 1'b0;
+        CA <= 1'b0;
+
+      end
+      5'd10: begin
+        // b0001000
+        CG <= 1'b0;
+        CF <= 1'b0;
+        CE <= 1'b0;
+        CD <= 1'b1;
+        CC <= 1'b0;
+        CB <= 1'b0;
+        CA <= 1'b0;
+      end
+      5'd11: begin
+        // b0000011
+        CG <= 1'b0;
+        CF <= 1'b0;
+        CE <= 1'b0;
+        CD <= 1'b0;
+        CC <= 1'b0;
+        CB <= 1'b1;
+        CA <= 1'b1;
+      end
+      5'd12: begin
+        // b1000110
+        CG <= 1'b1;
+        CF <= 1'b0;
+        CE <= 1'b0;
+        CD <= 1'b0;
+        CC <= 1'b1;
+        CB <= 1'b1;
+        CA <= 1'b0;
+      end
+      5'd13: begin
+        // b0100001
+        CG <= 1'b0;
+        CF <= 1'b1;
+        CE <= 1'b0;
+        CD <= 1'b0;
+        CC <= 1'b0;
+        CB <= 1'b0;
+        CA <= 1'b1;
+
+      end
+      5'd14: begin
+        // b0000110
+        CG <= 1'b0;
+        CF <= 1'b0;
+        CE <= 1'b0;
+        CD <= 1'b0;
+        CC <= 1'b1;
+        CB <= 1'b1;
+        CA <= 1'b0;
+      end
+      5'd15: begin
+        // b0001110
+        CG <= 1'b0;
+        CF <= 1'b0;
+        CE <= 1'b0;
+        CD <= 1'b1;
+        CC <= 1'b1;
+        CB <= 1'b1;
+        CA <= 1'b0;
+      end
+      5'd16: begin
+        // b1111111
+        CG <= 1'b1;
+        CF <= 1'b1;
+        CE <= 1'b1;
+        CD <= 1'b1;
+        CC <= 1'b1;
+        CB <= 1'b1;
+        CA <= 1'b1;
+      end
+    endcase
+  end
+
+endmodule
---
+++ src/top.v
@@ -0,0 +1,161 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/06/2023 03:50:55 PM
+// Design Name: 
+// Module Name: top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top (
+    input CLK100MHZ,
+    input CPU_RESETN,
+    input UART_TXD_IN,
+    inout JA,
+    inout [2:1] JD,
+    output UART_RXD_OUT,
+    output CA,
+    output CB,
+    output CC,
+    output CD,
+    output CE,
+    output CF,
+    output CG,
+    output DP,
+    output [7:0] AN
+);
+  parameter CLK_FREQUENCY = 100_000_000;
+  // parameter define
+  parameter CLK_FREQ = 100_000_000;  // sys clock frequency
+  parameter UART_BPS = 115200;  // uart bound frequency
+  localparam MAX_COUNTER = CLK_FREQUENCY / 10;  // 100ms needs number of counter  
+
+  reg  [31:0] counter;  // counter for 100ms
+
+
+  // wire define
+  wire        uart_recv_done;  // receive done flag
+
+  wire [63:0] uart_recv_data;  // receive data buff
+
+  reg         uart_send_en;  // send start flag
+
+
+  wire [63:0] uart_send_data;  // send data buff
+
+  wire        uart_tx_busy;  // when send is busy
+
+  wire [31:0] data;
+  wire        en;
+  assign en = 1'b1;
+
+  wire [63:0] desOut;
+  wire [63:0] desIn;
+  wire [55:0] key = 56'hffff_ffff_ffff_ff;
+
+  wire decrypt;
+  assign decrypt = 1'b0;
+
+  assign uart_send_data = desOut;
+
+
+  always @(posedge CLK100MHZ or negedge CPU_RESETN) begin
+    if (!CPU_RESETN) begin
+      counter <= 32'b0;
+      uart_send_en <= 1'b0;
+    end else if (counter < MAX_COUNTER - 1) begin
+      counter <= counter + 1'b1;
+      uart_send_en <= 1'b0;
+    end else begin
+      counter <= 32'b0;
+      uart_send_en <= 1'b1;
+    end
+  end
+
+  led #(
+      .CLK_FREQUENCY(CLK_FREQUENCY)
+  ) led_inst (
+      .clk (CLK100MHZ),
+      .en  (en),
+      .data(desOut[31:0]),
+      .CA  (CA),
+      .CB  (CB),
+      .CC  (CC),
+      .CD  (CD),
+      .CE  (CE),
+      .CF  (CF),
+      .CG  (CG),
+      .DP  (DP),
+      .AN  (AN)
+  );
+
+
+  des des_inst (
+      .desOut(desOut),
+      .desIn (desIn),
+      .key   (key),
+      .decrypt(decrypt),
+      .clk   (CLK100MHZ)
+  );
+
+  ds18b20_dri ds18b20_dri_inst (
+      .clk      (CLK100MHZ),
+      .rst_n    (CPU_RESETN),
+      .dq       (JA),
+      .temp_data(desIn[19:0])
+  );
+
+  // uart receive module
+  //   uart_recv_b8 #(
+  //       .CLK_FREQ(CLK_FREQ),
+  //       .UART_BPS(UART_BPS)
+  //   ) uart_recv_b8_inst (
+  //       .sys_clk  (CLK100MHZ),
+  //       .sys_rst_n(CPU_RESETN),
+
+  //       .uart_rxd (UART_TXD_IN),
+  //       .uart_done(uart_recv_done),
+  //       .uart_data(uart_recv_data)
+  //   );
+
+  // uart send module
+  uart_send_b8 #(
+      .CLK_FREQ(CLK_FREQ),
+      .UART_BPS(UART_BPS)
+  ) uart_send_b8_inst (
+      .sys_clk  (CLK100MHZ),
+      .sys_rst_n(CPU_RESETN),
+
+      .uart_en     (uart_send_en),
+      .uart_din    (uart_send_data),
+      .uart_tx_busy(uart_tx_busy),
+      .uart_txd    (JD[2])
+  );
+
+  // uart loop module
+  //   uart_loop uart_loop_inst (
+  //       .sys_clk  (CLK100MHZ),
+  //       .sys_rst_n(CPU_RESETN),
+
+  //       .recv_done(uart_recv_done),
+  //       .recv_data(uart_recv_data),
+
+  //       .tx_busy  (uart_tx_busy),
+  //       .send_en  (uart_send_en),
+  //       .send_data(uart_send_data)
+  //   );
+
+endmodule  //top
